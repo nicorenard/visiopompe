cd2ea6f221be8f66eb203bd5785c292cc8f38f27
Nicolas Renard <nicolas.renard@univ-brest.fr>
2022-10-21 09:37:06 +0200

    minor bug UX corrected

--- src/config/settings.py
+++ src/config/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'visiopompe',
-        'USER': 'visiopompe',
-        'PASSWORD': 'visiopompe',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'visiopompe',
+        #'USER': 'visiopompe',
+        #'PASSWORD': 'visiopompe',
+        #'HOST': 'localhost',
+        #'PORT': '',
     }
 }
 
