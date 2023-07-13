16aca511d92b82b203481ee1e5b984f773918a95
nicolas.renard <nicolas.renard@univ-brest.fr>
2023-07-13 10:58:35 +0200

    v1.2 ajout filtre de recherche pour documentation bug fixed

--- src/pompe/static/pompe/js/filtres.js
+++ src/pompe/static/pompe/js/filtres.js
@@ -1,17 +1,28 @@
-//
+// filtre fabriquant
 function filtreFabriquant() {
   var input, filter, tr, i;
   input = document.getElementById('recherche');
   filter = input.value.toUpperCase();
   tr = document.getElementsByTagName("tr");
 
-  // boucle basé sur la recherche par nom de classe d'element.
-  //https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName
+  // boucle basée sur la recherche par nom de classe d'élément.
+  // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName
   for (i = 0; i < tr.length; i++) {
-    tdFabriquant = tr[i].getElementsByClassName("nom")[0];
+    // récupère les td avec les images
+    var tdFabriquant = tr[i].getElementsByClassName("nom")[0];
     if (tdFabriquant) {
-      txtValueNom = tdFabriquant.textContent || tdFabriquant.innerText;
-      if (txtValueNom.toUpperCase().indexOf(filter) > -1) {
+      // on récupère toutes les images dans les td
+      var fabs = tdFabriquant.querySelectorAll('img');
+      var found = false;
+      for (var j = 0; j < fabs.length; j++) {
+        // on regarde si les attributs alt sont identiques au filtre que l'on recherche
+        if (fabs[j].getAttribute('alt').toUpperCase() === filter) {
+          found = true;
+          break;
+        }
+      }
+      // on affiche ou masque l'élément du tableau en fonction du résultat de recherche
+      if (found) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
